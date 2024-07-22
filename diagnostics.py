41653fd810d8c0fd2b2d84f6875c724f71f9fb27
mehulakumawat <mehul.kumawat@stellantis.com>
2024-07-22 23:42:22 +0530

    diagnostics code done py

--- diagnostics.py
+++ diagnostics.py
@@ -1,45 +1,89 @@
-
-import pandas as pd
-import numpy as np
-import timeit
-import os
 import json
+import os
+import pickle
+import subprocess
+import timeit
 
-##################Load config.json and get environment variables
-with open('config.json','r') as f:
-    config = json.load(f) 
+import numpy as np
+import pandas as pd
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-test_data_path = os.path.join(config['test_data_path']) 
+from training import prepare_data
 
-##################Function to get model predictions
+# Load config.json and get environment variables
+with open("config.json", "r", encoding="utf-8") as config_file:
+    config = json.load(config_file)
+
+dataset_csv_path = os.path.join(config["output_folder_path"])
+test_data_path = os.path.join(config["test_data_path"])
+
+
+# Function to get model predictions
 def model_predictions():
-    #read the deployed model and a test dataset, calculate predictions
-    return #return value should be a list containing all predictions
+    # read the deployed model and a test dataset, calculate predictions
+    with open(
+        os.path.join(config["prod_deployment_path"], "trainedmodel.pkl"), "rb"
+    ) as f:
+        model = pickle.load(f)
 
-##################Function to get summary statistics
+    test_df = pd.read_csv(os.path.join(test_data_path, "testdata.csv"))
+    X, y = prepare_data(test_df)
+
+    y_pred = model.predict(X)
+
+    y_pred = y_pred.flatten()
+    assert y_pred.shape[0] == X.shape[0]
+
+    print(y_pred.tolist())
+    # return value should be a list containing all predictions
+    return y_pred.tolist()
+
+
+# Function to get summary statistics
 def dataframe_summary():
-    #calculate summary statistics here
-    return #return value should be a list containing all summary statistics
+    # calculate summary statistics here
 
-##################Function to get timings
+    df = pd.read_csv(os.path.join(dataset_csv_path, "finaldata.csv"))
+
+    summary_df = df.describe(include="number")
+    summary_df = summary_df.loc[["mean", "std", "50%"], :]
+
+    print(summary_df.isnull().mean().values.tolist())
+    print(summary_df)
+    # return value should be a list containing all summary statistics
+
+    return summary_df
+
+
+# Function to get timings
 def execution_time():
-    #calculate timing of training.py and ingestion.py
-    return #return a list of 2 timing values in seconds
+    # calculate timing of training.py and ingestion.py
+    time_list = []
+    start_time = timeit.default_timer()
+    os.system("python ingestion.py")
+    end_time = timeit.default_timer()
+    print(f"Ingestion process took: {end_time-start_time:.2f} seconds")
+    time_list.append(end_time - start_time)
+    start_time = timeit.default_timer()
+    os.system("python training.py")
+    end_time = timeit.default_timer()
+    print(f"Training process took: {end_time-start_time:.2f} seconds")
+    time_list.append(end_time - start_time)
+    print(time_list)
+    return time_list  # return a list of 2 timing values in seconds
 
-##################Function to check dependencies
+
+# Function to check dependencies
 def outdated_packages_list():
-    #get a list of 
+    # get a list of
+    outdated_pkgs = subprocess.check_output(["pip", "list", "--outdated"])
+    print(outdated_pkgs)
+
+    with open("outdated_pkgs.txt", "wb") as file:
+        file.write(outdated_pkgs)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     model_predictions()
     dataframe_summary()
     execution_time()
     outdated_packages_list()
-
-
-
-
-
-    
